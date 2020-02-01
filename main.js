e080a6eb456e5e33668596aa6673fb1c605cb966
Theodav <theodore.d.avray@efrei.net>
2020-02-01 19:33:12 +0100

    Radical change : from TS to JS for Glitch production

---
+++ main.js
@@ -0,0 +1,42 @@
+import * as Phaser from 'phaser'
+import { WheelScene } from './wheel'
+import { MenuScene } from './menu'
+
+// the game itself
+let game
+
+// once the window loads...
+window.onload = function () {
+  // game configuration object
+  const gameConfig = {
+
+    autoStart: true,
+    scene: [MenuScene, WheelScene],
+
+    // resolution and scale mode
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      parent: 'thegame',
+      width: 1900,
+      height: 900
+    },
+    dom: {
+      createContainer: true
+    },
+
+    // game background color
+    backgroundColor: 0x000000
+  }
+
+  // game constructor
+  game = new Phaser.Game(gameConfig)
+
+  // game.scene.add('menuScene', MenuScene);
+  // game.scene.add('wheelScene', WheelScene);
+
+  // game.scene.start('menuScene');
+
+  // pure javascript to give focus to the page/frame
+  window.focus()
+}
